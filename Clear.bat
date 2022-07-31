bac5369bf7b5e771bb3874bba7bd93dcb3c57b95
Istar Eldritch <me@ruben.io>
2022-07-31 18:22:18 +0100

    Add setup scripts

---
+++ Clear.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cd /D "%~dp0"
+
+IF exist "P:\PDArp\" (
+	echo Removing existing link P:\PDArp
+	rmdir "P:\PDArp\"
+)
+
+IF exist "C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp\" (
+	echo Removing existing link C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp
+	rmdir "C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp\"
+)
+
+echo Done
---
+++ Setup.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cd /D "%~dp0"
+
+IF exist "P:\PDArp\" (
+	echo Removing existing link P:\PDArp
+	rmdir "P:\PDArp\"
+)
+
+echo Creating link P:\PDArp
+mklink /J "P:\PDArp\" "%cd%\PDArp\"
+
+IF exist "C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp\" (
+	echo "Removing existing link C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp"
+	rmdir "C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp\"
+)
+
+echo Creating link C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp\
+mklink /J "C:\Program Files (x86)\Steam\steamapps\common\DayZ\@PDArp" "%cd%"
+
+echo Done
